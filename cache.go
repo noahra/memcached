462218d3e481c2a08f7a8980212c933e1fbbf1b5
rahimz <rahimz@kth.se>
2024-08-23 22:04:28 +0200

    changes

--- cache.go
+++ cache.go
@@ -1,6 +1,28 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
+
+type CacheValue struct {
+	Key           string
+	Flags         string
+	ExpiryTime    float64
+	AmountOfBytes int
+	DataBlock     string
+	CreatedAt     time.Time
+}
+
+func ExpiryCheck(cacheValue CacheValue) bool {
+	if cacheValue.ExpiryTime != 0 {
+		duration := time.Now().Sub(cacheValue.CreatedAt)
+		if duration.Seconds() > cacheValue.ExpiryTime {
+			return true
+		}
+	}
+	return false
+}
 
 type CacheStore interface {
 	Get(key string) (CacheValue, bool)
--- cache_test.go
+++ cache_test.go
@@ -25,3 +25,16 @@ func TestCacheOperations(t *testing.T) {
 		t.Error("key does exist")
 	}
 }
+
+func TestCacheUtils(t *testing.T) {
+	cacheValue := CacheValue{ExpiryTime: 1}
+	hasExpired := ExpiryCheck(cacheValue)
+	if !hasExpired {
+		t.Error("HasExpired should return false")
+	}
+	cacheValue = CacheValue{ExpiryTime: 0}
+	hasExpired = ExpiryCheck(cacheValue)
+	if hasExpired {
+		t.Error("HasExpired should return true")
+	}
+}

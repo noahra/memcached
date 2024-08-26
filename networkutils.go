da8151ed32b84564ef687c4de7345b1972726fa3
rahimz <rahimz@kth.se>
2024-08-26 16:08:24 +0200

    message framing introduced

--- networkutils.go
+++ networkutils.go
@@ -6,26 +6,76 @@ import (
 )
 
 func HandleConnection(conn net.Conn, memcache *Cache) error {
+	buf := make([]byte, 1024)
+	totalBytesRead := 0
+	commands := map[string]struct{}{
+		"set":     {},
+		"add":     {},
+		"append":  {},
+		"prepend": {},
+		"replace": {},
+		"get":     {},
+	}
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+
+		bytesRead, err := conn.Read(buf[totalBytesRead:])
 		if err != nil {
 			return err
-
 		}
+		totalBytesRead += bytesRead - 2
 
-		words := strings.Fields(string(buf))
-		if len(words) < 2 {
+		words := strings.Fields(string(buf[:totalBytesRead]))
+
+		if len(words) > 1 && !containsCommand(words[0], commands) {
+			buf = make([]byte, 1024)
+			totalBytesRead = 0
 			continue
 		}
 
-		command, err := CreateCommand(words, conn)
-		if err != nil {
-			return err
+		if len(words) > 6 {
+			buf = make([]byte, 1024)
+			totalBytesRead = 0
+			continue
 		}
-		err = command.Execute(memcache)
-		if err != nil {
-			return err
+
+		if len(words) == 2 && words[0] == "get" {
+			_, ok := memcache.Get(words[1])
+			if !ok {
+				continue
+			}
+			command, err := CreateCommand(words, conn)
+			if err != nil {
+				return err
+			}
+			err = command.Execute(memcache)
+			if err != nil {
+				return err
+			}
+			buf = make([]byte, 1024)
+			totalBytesRead = 0
+			continue
+		}
+
+		if len(words) >= 5 && containsCommand(words[0], commands) {
+			if len(words) == 6 && words[len(words)-1] != "noreply" {
+				continue
+			}
+			command, err := CreateCommand(words, conn)
+			if err != nil {
+				return err
+			}
+			err = command.Execute(memcache)
+			if err != nil {
+				return err
+			}
+			buf = make([]byte, 1024)
+			totalBytesRead = 0
+			continue
 		}
 	}
+}
+
+func containsCommand(word string, commands map[string]struct{}) bool {
+	_, exists := commands[word]
+	return exists
 }
